93bab4c278d5430924cc872df346fa33656f277e
nerdfavour <favourackonu13@gmail.com>
2024-02-07 21:22:21 +0000

    parsing json

--- jsonFiles/jsonUse.js
+++ jsonFiles/jsonUse.js
@@ -1,3 +1,4 @@
+//stringify
 const names = ["Father", "son", "Holy Spirit"];
 
 const person = {
@@ -34,3 +35,44 @@ const jsonString3 = JSON.stringify(people);
 console.log(jsonString);
 console.log(jsonString2);
 console.log(jsonString3);
+
+//parse
+
+const names2 = `["Father", "son", "Holy Spirit"]`;
+
+const person2 = `{
+    "name": "Gojo",
+    "age": 28,
+    "isEmoloyed": true,
+    "hobbies": "none"
+}`;
+
+const people2 = `[
+    {
+        "name": "Gojo",
+        "age": 28,
+        "isEmoloyed": true,
+        "hobbies": "none"
+    },
+    {
+        "name": "Geto",
+        "age": 28,
+        "isEmoloyed": true,
+        "hobbies": "none"
+    },
+    {
+        "name": "Utahime",
+        "age": 28,
+        "isEmoloyed": true,
+        "hobbies": "none"
+    }
+]`
+
+const jsonParse = JSON.parse(names2);
+const jsonParse2 = JSON.parse(person2);
+const jsonParse3 = JSON.parse(people2);
+console.log(jsonParse);
+console.log(jsonParse2);
+console.log(jsonParse3);
+
+//note the backticks make it a string which we then parse
